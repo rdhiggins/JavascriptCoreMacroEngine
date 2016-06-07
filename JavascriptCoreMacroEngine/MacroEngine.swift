5ba9dea482ff4c6508b513a8c0d04c363a1267de
Rodger Higgins <rdhiggins@gmail.com>
2016-06-06 20:01:57 -0500

    Added MacroMethod class

--- JavascriptCoreMacroEngine/MacroEngine.swift
+++ JavascriptCoreMacroEngine/MacroEngine.swift
@@ -18,7 +18,6 @@ protocol MacroEngineSupport {
 class MacroEngine {
     static let sharedInstance: MacroEngine = MacroEngine()
     
-    
     let context: JSContext = JSContext()
     
     
@@ -30,14 +29,48 @@ class MacroEngine {
     func addObject(object: MacroEngineSupport) {
         object.addMacroSupport(self)
     }
-    
+
+
     func insertBlockAsObject<BlockType>(block: BlockType, key: String) {
         context.setObject(unsafeBitCast(block, AnyObject.self), forKeyedSubscript: key)
     }
-    
+
+
+    func setMethod(javascript: String, key: String) -> JSValue! {
+        context.evaluateScript(javascript)
+
+        return context.objectForKeyedSubscript(key)
+    }
+
+
     private func setupExceptionHandler() {
         context.exceptionHandler = { context, error in
             print("JS Exception: \(error)")
         }
+    }
+}
+
+
+
+
+class MacroMethod {
+
+    private let engine = MacroEngine.sharedInstance
+    private let jsValue: JSValue!
+
+    var javascript: String
+    let key: String
+
+
+    init(javascript: String, key: String) {
+        self.key = key
+        self.javascript = javascript
+
+        jsValue = engine.setMethod(javascript, key: key)
+    }
+
+
+    func call(params: [AnyObject]!) -> JSValue! {
+        return jsValue.callWithArguments(params)
     }
 }
