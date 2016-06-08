f2094d1a0401c19a2405f90a8b532288db83c34e
Rodger Higgins <rdhiggins@gmail.com>
2016-06-07 20:05:28 -0500

    Got line numbers working.  Also can now set the text color and font.

--- JavascriptCoreMacroEngine/LineNumberLayoutManager.swift
+++ JavascriptCoreMacroEngine/LineNumberLayoutManager.swift
@@ -10,4 +10,50 @@ import UIKit
 
 class LineNumberLayoutManager: NSLayoutManager {
 
+    var textColor: UIColor = UIColor.whiteColor() {
+        didSet {
+            setupFontAttributes()
+        }
+    }
+
+    var font: UIFont! = UIFont.monospacedDigitSystemFontOfSize(10.0, weight: UIFontWeightThin)
+
+    private var fontAttributes: [String : NSObject] = [
+        NSFontAttributeName : UIFont.monospacedDigitSystemFontOfSize(10.0, weight: UIFontWeightThin),
+        NSForegroundColorAttributeName : UIColor.whiteColor()
+    ]
+
+
+    private func setupFontAttributes() {
+        fontAttributes = [
+            NSFontAttributeName : font,
+            NSForegroundColorAttributeName: textColor
+        ]
+    }
+
+    override func processEditingForTextStorage(textStorage: NSTextStorage, edited editMask: NSTextStorageEditActions, range newCharRange: NSRange, changeInLength delta: Int, invalidatedRange invalidatedCharRange: NSRange) {
+        super.processEditingForTextStorage(textStorage, edited: editMask, range: newCharRange, changeInLength: delta, invalidatedRange: invalidatedCharRange)
+    }
+
+
+    override func drawBackgroundForGlyphRange(glyphsToShow: NSRange, atPoint origin: CGPoint) {
+        super.drawBackgroundForGlyphRange(glyphsToShow, atPoint: origin)
+
+        enumerateLineFragmentsForGlyphRange(glyphsToShow) {
+            rect, usedRect, textContainer, glyphRange, stop in
+
+            let charRange = self.characterRangeForGlyphRange(glyphRange, actualGlyphRange: nil)
+
+            let before = (self.textStorage!.string as NSString).substringToIndex(charRange.location)
+            let lineNumber = before.componentsSeparatedByCharactersInSet(NSCharacterSet.newlineCharacterSet()).count
+
+            let r = CGRect(x: 0, y: rect.origin.y, width: 40.0, height: rect.size.height)
+            let gutterRect = CGRectOffset(r, origin.x, origin.y)
+            let s = String(format: "%ld", lineNumber)
+            let size = s.sizeWithAttributes(self.fontAttributes)
+            let ds = CGRectOffset(gutterRect, CGRectGetWidth(gutterRect) - 4 - size.width, (CGRectGetHeight(gutterRect) - size.height) / 2.0)
+
+            s.drawInRect(ds, withAttributes: self.fontAttributes)
+        }
+    }
 }
