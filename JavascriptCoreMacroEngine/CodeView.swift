c5cdf34d41159e0b87c625b49a5159b0419dd92d
Rodger Higgins <rdhiggins@gmail.com>
2016-06-08 19:08:28 -0500

    Code cleanup

--- JavascriptCoreMacroEngine/CodeView.swift
+++ JavascriptCoreMacroEngine/CodeView.swift
@@ -25,9 +25,15 @@
 
 import UIKit
 
+
+/// A custom UIView based class that contains a custom UITextView with
+/// a custom layout manager.  Use this class to show a text file with a line
+/// number gutter on the left.
 @IBDesignable
 class CodeView: UIView {
-    
+
+
+    /// Property containing the code to display
     @IBInspectable var text: String? {
         set {
             textView?.text = newValue
@@ -37,62 +43,82 @@ class CodeView: UIView {
         }
     }
 
+
+    /// Property containing the width of the gutter to display the line
+    /// numbers in.
     @IBInspectable var gutterWidth: CGFloat = 40.0 {
         didSet {
             textView?.gutterWidth = gutterWidth
             layoutManager?.gutterWidth = gutterWidth
         }
     }
-    
+
+
+    /// Property containing the width of the gutter border
     @IBInspectable var gutterBorderWidth: CGFloat = 1.0 {
         didSet {
             textView?.gutterStrokeWidth = gutterBorderWidth
         }
     }
-    
+
+
+    /// Property containing the background color for the gutter
     @IBInspectable var gutterBackgroundColor: UIColor = UIColor.grayColor() {
         didSet {
             textView?.gutterFillColor = gutterBackgroundColor
         }
     }
-    
+
+
+    /// property containing the color to use for the line numbers
     @IBInspectable var gutterTextColor: UIColor = UIColor.blackColor() {
         didSet {
             textView?.gutterTextColor = gutterTextColor
             layoutManager?.textColor = gutterTextColor
         }
     }
-    
+
+
+    /// Property containing the color og the gutter border
     @IBInspectable var gutterBorderColor: UIColor = UIColor.blackColor() {
         didSet {
             textView?.gutterBorderColor = gutterBorderColor
         }
     }
-    
+
+
+    /// Property containing the color to use for the text displayed
     @IBInspectable var textColor: UIColor? {
         didSet {
             textView?.textColor = textColor
         }
     }
-    
-    @IBInspectable var font: UIFont? = UIFont.monospacedDigitSystemFontOfSize(14.0, weight: UIFontWeightThin) {
+
+
+    ///  Property containing the font to use for the code and line numbers
+    @IBInspectable var font: UIFont? =
+        UIFont.monospacedDigitSystemFontOfSize(14.0, weight: UIFontWeightThin) {
         didSet {
             textView?.font = font
             layoutManager?.font = font
         }
     }
-    
+
+
+    /// Property enabling editing support for the text view
     @IBInspectable var canEdit: Bool = true {
         didSet {
             textView?.editable = canEdit
         }
     }
 
+
     var textContainer: NSTextContainer!
     var layoutManager: LineNumberLayoutManager!
     var textStorage: NSTextStorage!
     var textView: LineNumberTextView!
-    
+
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
@@ -105,8 +131,9 @@ class CodeView: UIView {
         setup()
     }
     
-    
-    func setup() {
+
+    /// A private method for setting up this view
+    private func setup() {
         textContainer = NSTextContainer()
         
         layoutManager = LineNumberLayoutManager()
@@ -119,14 +146,20 @@ class CodeView: UIView {
         setupTextView()
     }
     
-    
+
+    /// A private method used to setup the gutter region.  This entails
+    /// defining the exclusion zone used for the gutter
     private func setupGutterRegion() {
-        let bp = UIBezierPath(rect: CGRect(x: CGFloat(0.0), y: CGFloat(0.0), width: gutterWidth, height: CGFloat(FLT_MAX)))
+        let bp = UIBezierPath(rect: CGRect(x: CGFloat(0.0),
+                                 y: CGFloat(0.0),
+                             width: gutterWidth,
+                            height: CGFloat(FLT_MAX)))
         
         self.textContainer.exclusionPaths = [bp]
     }
     
-    
+
+    /// A private method for initializing the textview used.
     private func setupTextView() {
         textView = LineNumberTextView(frame: self.bounds, textContainer: textContainer)
         textView.text = text
@@ -140,7 +173,8 @@ class CodeView: UIView {
         
         addSubview(textView)
     }
-    
+
+
     override func layoutSubviews() {
         super.layoutSubviews()
         
