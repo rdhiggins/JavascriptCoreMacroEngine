026090100462ecc3204b8982afd5942bdf214d60
Rodger Higgins <rdhiggins@gmail.com>
2016-06-08 19:36:24 -0500

    Code cleanup

--- JavascriptCoreMacroEngine/ViewController.swift
+++ JavascriptCoreMacroEngine/ViewController.swift
@@ -26,85 +26,111 @@ import UIKit
 import JavaScriptCore
 
 
-class ViewController: UIViewController, ProgressUpdate, MacroEditResults {
+/// The main view controller for the demo application
+class ViewController: UIViewController, ProgressUpdate {
 
     @IBOutlet weak var innerRingView: CustomProgressView!
     @IBOutlet weak var middleRingView: CustomProgressView!
     @IBOutlet weak var outerRingView: CustomProgressView!
     @IBOutlet weak var codeView: CodeView!
 
+    /// A private property containing the four MacroMethod objects used
+    /// for the buttons to change the values of the progress indicators
     private var macros: [MacroMethod] = []
+
+    private var javascriptFiles: [String] = [
+        "evenLess",
+        "less",
+        "more",
+        "evenMore"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        let thing = Thing.sharedInstance
-        innerRingView.progress = CGFloat(thing.innerProgress)
-        middleRingView.progress = CGFloat(thing.middleProgress)
-        outerRingView.progress = CGFloat(thing.outerProgress)
 
+        // Setup the delegate
+        let thing = Thing.sharedInstance
         thing.delegate = self
 
         registerJavascriptMethods()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+
+        // Get the current values of the progress indicators
+        let thing = Thing.sharedInstance
+        innerRingView.progress = CGFloat(thing.innerProgress)
+        middleRingView.progress = CGFloat(thing.middleProgress)
+        outerRingView.progress = CGFloat(thing.outerProgress)
     }
 
 
+    /// IBAction used by the progress indicator buttons
     @IBAction func executeMacroButton(sender: UIButton) {
+
+        // Call the javascript method
         macros[sender.tag].call()
+
+        // Load the javascript for the button just executed
         codeView?.text = macros[sender.tag].javascript
     }
     
 
+    /// A private method used to register the javascript methods
+    /// with the macro engine
     private func registerJavascriptMethods() {
-        macros.append(loadJavascriptFile("evenLess"))
-        macros.append(loadJavascriptFile("less"))
-        macros.append(loadJavascriptFile("more"))
-        macros.append(loadJavascriptFile("evenMore"))
+        for file in javascriptFiles {
+            macros.append(loadJavascriptFile(file))
+        }
     }
 
+
+    /// ProgressUpdate protocol method called when the inner progress has 
+    /// changed
     func innerProgressUpdate(progress: Float) {
         innerRingView.progress = CGFloat(progress)
     }
 
+
+    /// ProgressUpdate protocol method called when the middle progress has
+    /// changed
     func middleProgressUpdate(progress: Float) {
         middleRingView.progress = CGFloat(progress)
     }
 
+
+    /// ProgressUpdate protocol method called when the outer progress 
+    /// has changed
     func outerProgressUpdate(progress: Float) {
         outerRingView.progress = CGFloat(progress)
     }
-    
-    @IBAction func editButton(sender: UIButton) {
-    }
-    
+
+
+    /// A private method that is called to load contents of the 
+    /// javascript file included in the resource bundle of the application
     private func loadJavascriptFile(key: String) -> MacroMethod {
         guard let filePath = NSBundle.mainBundle().pathForResource(key, ofType: "js") else {
             print("Unable to load javascript file \(key).js")
             fatalError()
         }
         
-        let script = try! String(contentsOfFile: filePath, encoding: NSUTF8StringEncoding)
+        let script = try! String(contentsOfFile: filePath,
+                                 encoding: NSUTF8StringEncoding)
         
         return MacroMethod(javascript: script, key: key)
     }
-    
+
+
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "EditMacros" {
-            if let mevc = segue.destinationViewController as? MacroEditViewController {
+            if let mevc = segue.destinationViewController
+                as? MacroEditViewController {
+
                 mevc.macros = macros
-                
-                mevc.delegate = self
             }
         }
-    }
-    
-    func results(macros: [MacroMethod]) {
     }
 }
 
